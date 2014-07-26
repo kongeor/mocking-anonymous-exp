a6e09657a24aa096fef4b659ea893f6d9cfd8972
psy <psy@psy-VirtualBox>
2014-07-26 16:44:34 +0300

    Prepare the WormServiceTest

--- src/test/java/io/github/kongeor/mocking/anonymous/service/WormServiceTest.java
+++ src/test/java/io/github/kongeor/mocking/anonymous/service/WormServiceTest.java
@@ -1,14 +1,47 @@
 package io.github.kongeor.mocking.anonymous.service;
 
-import static org.junit.Assert.fail;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+import io.github.kongeor.mocking.anonymous.filter.HeroWormFilter;
+import io.github.kongeor.mocking.anonymous.model.Worm;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(WormService.class) // this is not right!
 public class WormServiceTest {
 
+	private List<Worm> worms;
+	private @Mock Worm worm;
+	private @Mock RemoteService remoteService;
+	private @Mock HeroWormFilter heroWormFilter;
+	
+	private WormService wormService;
+
+	@Before
+	public void setUp() throws Exception {
+		worms = new ArrayList<>();
+		worms.add(worm);
+		
+		whenNew(HeroWormFilter.class).withArguments(worm, remoteService)
+				.thenReturn(heroWormFilter);
+
+		wormService = new WormService(worms, remoteService);
+	}
+	
 	@Test
 	public void testGetHeroes() {
-		fail("Well, you do know what to do ...");
+		when(heroWormFilter.isHero()).thenReturn(true);
+		assertThat(wormService.getHeroes()).containsExactly(worm);
 	}
 
 }
